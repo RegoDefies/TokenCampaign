54931c117d419c83d53487379d526e7b0cf8b422
Rego <pvrego@gmail.com>
2021-12-19 15:08:26 -0300

    Update github file from gitbook repo

--- _fetch_from_gitbook__TokenCampaign.bat
+++ _fetch_from_gitbook__TokenCampaign.bat
@@ -1,6 +1,6 @@
 cd ..\SNG.Gitbook\blockchain-projects
 git pull
-copy tokencampaign.md ..\SNG.TokenCampaign\README.md 
-cd ..\SNG.TokenCampaign
+copy tokencampaign.md ..\..\SNG.TokenCampaign\README.md 
+cd ..\..\SNG.TokenCampaign
 git commit -a -m "Update github file from gitbook repo"
 git push
